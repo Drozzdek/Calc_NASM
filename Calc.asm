358590d13bd69d9901ddc0b220412c86fb0763db
Shiki93 <murillo.tec@gmail.com>
2017-05-04 19:00:03 -0600

    Se crea una nueva version
    Se crea una nueva version que incluya operaciones con numeros en diferentes bases.

--- Calc.asm
+++ Calc.asm
@@ -1,128 +1,125 @@
 %include 'io.mac'
 
 .DATA
-msg_Inicio		db		'Ingrese una expresion: ', 0
-msg_Error		db		'Error', 0
-msg_Final		db		'Todo Funciona!', 0
+msg_Inicio		db		'Ingrese una expresion: ',0
+msg_Final		db		'La respuesta es: ',0
+msg_Error		db		'Error. Caracter invalido.',0
+nueva_prec		db		0
+pila_prec		db		0
+cont 			db		0
 largo			db		0
-operadores 		db		'+-*/'
 
 .UDATA
-expresion		resb	32
-A_evaluar		resb	42
+expresion		resb	64
+A_evaluar		resb	128
 
 .CODE
 	.STARTUP
-	PutStr	msg_Inicio
-	GetStr  expresion
-	
-	mov EBX, expresion			;
-	xor EDI,EDI					;Vacia el registro
-	xor ESI,ESI					;Vacia el registro
-	
-Ciclo:
-	cmp byte[EBX], 0
-	je  Done
-	;Error si es mayor que z minuscula
-	;Error si esta en el hueco entre z minuscula y a mayuscula
-	;Error si esta entre 9 y a mayuscula, Pero verificar que no sea un igual
-	;Error si es menor a 0 y diferente de un operador valido
-	;+-*/()
-	cmp byte[EBX], 'b'
-	je NoError
-	cmp byte[EBX], 'd'
-	je NoError
-	cmp byte[EBX], 'h'
-	je NoError
-	cmp byte[EBX], 'o'
-	je NoError
-	cmp byte[EBX], '9'
-	jb Pos_Error
-	cmp byte[EBX], 'z'
-	ja Error
-	cmp byte[EBX], 'a'
-	jb Pos_Error
-	cmp byte[EBX], 'A'
-	jb Pos_Error
-	;es un operando
-	;ciclo, hasta encontrar operador
-	mov byte [A_evaluar+EDI], '@'
-	inc EDI
-	
-cicloOperando:
-	mov AL, byte[EBX]
-	mov byte [A_evaluar+EDI], AL
-	inc EBX
-	cmp byte[EBX], '0'
-	jb  operador
-	cmp byte[EBX], '9'
+	push EBP
+	mov EBP,ESP
+	PutStr msg_Inicio
+	GetStr expresion
+	mov EBX, expresion
+	XOR EDI,EDI
+;#############################################################################################################
+cicloParser:
+	XOR EAX,EAX
+	XOR ECX,ECX
+	XOR EDX,EDX
+	XOR ESI,ESI
+	cmp byte[EBX],0
+	je	Respuesta
+	cmp byte[EBX],'('
+	jb	Error
+	cmp byte[EBX],'b'
+	je	Bin
+	cmp byte[EBX],'h'
+	je	Hex
+	cmp byte[EBX],'o'
+	je	Oct
+	cmp byte[EBX],'F'
 	ja	Error
-	
-operador:		
-	mov ESI, operadores			;Consigue el inicio de operadores. ESI porque es uno de los registros que permiten buscar dentro de la memoria.
-	call Prioridad
-
-Error:
-	PutStr msg_Error
-	nwln
-	mov eax, 1
-	mov ebx, 1
-	int 0x80
-
-Pos_Error:
-	cmp byte[EBX], 'Z'
-	ja Error
-	cmp byte[EBX], '='
-	je NoError
-	cmp byte[EBX], '9'
-	ja Error
-	cmp byte[EBX], '/'
-	jae NoError
-	cmp byte[EBX], '-'
-	je NoError
-	cmp byte[EBX], '+'
-	je NoError
-	cmp byte[EBX], '*'
-	je NoError
-	cmp byte[EBX], '('
-	je NoError
-	cmp byte[EBX], ')'
-	je NoError
+	cmp byte[EBX],'0'
+	jb	Operador
+	cmp byte[EBX],'9'
+	ja	N_Hex
+	jmp N_Val
+N_Hex:
+	cmp byte[EBX],'A'
+	je N_Val
+	cmp byte[EBX],'B'
+	je N_Val
+	cmp byte[EBX],'C'
+	je N_Val
+	cmp byte[EBX],'D'
+	je N_Val
+	cmp byte[EBX],'E'
+	je N_Val
+	cmp byte[EBX],'F'
+	je N_Val
 	jmp Error
-
-NoError:
-Num_Letra:
-	mov BL, byte[EBX]
-	mov A_evaluar, BL
+;#############################################################################################################
+Bin:
 	inc EBX
-	jmp Ciclo
-Simb:
-	mov A_evaluar, '@'
-	mov BL, byte[EBX]
-	mov A_evaluar, BL			;Aqui deberia pedir por la prioridad de operaciones y resolver de acuerdo a eso
+	jmp cicloParser
+Hex:
 	inc EBX
-	jmp Ciclo
+	jmp cicloParser
+Oct:
+	inc EBX
+	jmp cicloParser
+;#############################################################################################################
+N_Val:
+	mov byte[A_evaluar+EDI],'@'		;Se agrega un @ para que funcione como separador.
+	inc EDI							;Se incrementa el EDI porque es el que uso para moverme por la variable.
+cicloOperando:
+	XOR EAX,EAX						;EAX = 0. Limpia el registro de basura.
+	mov AL,byte[EBX]
+	mov byte[A_evaluar+EDI],AL
+	inc EDI
+	inc EBX
+	cmp byte[EBX],0
+	je	Respuesta
+	cmp byte[EBX],'0'
+	jb	Operador
+	cmp byte[EBX],'b'
+	je	Bin
+	cmp byte[EBX],'h'
+	je	Hex
+	cmp byte[EBX],'o'
+	je	Oct
+	cmp byte[EBX],'F'
+	ja	Error
+	cmp byte[EBX],'9'
+	jle	cicloOperando
+	ja	N_Hex2
+N_Hex2:
+	cmp byte[EBX],'A'
+	je cicloOperando
+	cmp byte[EBX],'B'
+	je cicloOperando
+	cmp byte[EBX],'C'
+	je cicloOperando
+	cmp byte[EBX],'D'
+	je cicloOperando
+	cmp byte[EBX],'E'
+	je cicloOperando
+	cmp byte[EBX],'F'
+	je cicloOperando
+	jmp Error
+;#############################################################################################################
+Operador:
+	inc EBX
+	jmp cicloParser
 
-Done:
+Respuesta:
 	PutStr	msg_Final
-	
-fin:
+	XOR EAX,EAX
+	pop EAX
+	PutLInt EAX
+	jmp Fin
+Error:
+	PutStr	msg_Error
+Fin:
 	nwln
 	.EXIT
-
-;Esta funcion me da la prioridad para la pila y decision de operadores
-Prioridad:
-	cmp AL, [operadores+ESI]		;El SI es el que tiene la prioridad del operador y va de 0 hasta donde la posicion donde la encontre.
-	jne Siguiente					;Si no lo a encontrado, salte al siguiente
-	ret								;De lo contrario, retorne a donde lo encontro
-Siguiente:
-	inc ESI
-	jmp Prioridad
-
-;Decide de acuerdo al algoritmo de postfijo que hacer con el operador conseguido
-;Meto a la pila si es mayor
-;Si es menor, lo saco, lo agrego a la expresion
-;Lo vuelvo a comparar
-;En postfijo hay que tener en cuenta los parenesis que en la expresion final no aparecen
-Ev_Operador:
-	
